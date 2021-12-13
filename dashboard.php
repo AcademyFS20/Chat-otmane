d3b3432a58b7840e3ca8dfb3fb800be08187ddfc
otmane1993 <otmomks@gmail.com>
2021-12-14 00:35:10 +0100

    Frontend en avance

---
+++ dashboard.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Dashboard</title>
+</head>
+<body id='dashboard'>
+    <header>
+        <div class="first">Profile</div>
+        <div class="second"></div>
+    </header>
+    <main>
+        <div id="profile">
+            <img src="image.png" class="rounded-circle">
+            <p>User name</p>
+            <a href="#">Change Name</a>
+            <a href="#">Change Password</a>
+            <p>110 User Online</p>
+        </div>
+        <div id="chat">
+            <div class="friend">
+                <div class="message">
+                    <div class="friend-line">
+                        <div class="img-message">
+                            <img src="image.png" class="rounded-circle">
+                            <div class="circle-green"></div>
+                        </div>
+                        <p>Tijani Abdelatif</p>
+                        <p>1 day ago</p>
+                    </div>
+                    <div>
+                        <p class="message-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum et ligula vulputate vehicula. Aenean pharetra arcu bibendum tempus bibendum. Vestibulum lacinia dui quis gravida ultricies. Sed et nulla in magna cursus finibus ac eu erat. Donec faucibus dignissim libero ut placerat. Duis vestibulum eget dolor varius sollicitudin.</p>
+                    </div>
+                </div>
+                <div class="message">
+                    <div class="friend-line">
+                        <div class="img-message">
+                            <img src="image.png" class="rounded-circle">
+                            <div class="circle-red"></div>
+                        </div>
+                        <p>Tijani Abdelatif</p>
+                        <p>1 day ago</p>
+                    </div>
+                    <div>
+                        <p class="message-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum et ligula vulputate vehicula. Aenean pharetra arcu bibendum tempus bibendum. Vestibulum lacinia dui quis gravida ultricies. Sed et nulla in magna cursus finibus ac eu erat. Donec faucibus dignissim libero ut placerat. Duis vestibulum eget dolor varius sollicitudin.</p>
+                    </div>
+                </div>
+                <div class="message">
+                    <div class="friend-line">
+                        <div class="img-message">
+                            <img src="image.png" class="rounded-circle">
+                            <div></div>
+                        </div>
+                        <p>Tijani Abdelatif</p>
+                        <p>1 day ago</p>
+                    </div>
+                    <div>
+                        <p class="message-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum et ligula vulputate vehicula. Aenean pharetra arcu bibendum tempus bibendum. Vestibulum lacinia dui quis gravida ultricies. Sed et nulla in magna cursus finibus ac eu erat. Donec faucibus dignissim libero ut placerat. Duis vestibulum eget dolor varius sollicitudin.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="me">
+                <div>
+                    <p>1 day ago</p>
+                    <div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum et ligula vulputate vehicula. Aenean pharetra arcu bibendum tempus bibendum.</p>
+                    </div>
+                </div>
+                <div>
+                    <p>1 day ago</p>
+                    <div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum et ligula vulputate vehicula. Aenean pharetra arcu bibendum tempus bibendum.</p>
+                    </div>
+                </div>
+                <div>
+                    <p>1 day ago</p>
+                    <div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum et ligula vulputate vehicula. Aenean pharetra arcu bibendum tempus bibendum.</p>
+                    </div>
+                </div>
+                <div>
+                    <p>1 day ago</p>
+                    <div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum et ligula vulputate vehicula. Aenean pharetra arcu bibendum tempus bibendum.</p>
+                    </div>
+                </div>
+                <div>
+                    <p>1 day ago</p>
+                    <div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum et ligula vulputate vehicula. Aenean pharetra arcu bibendum tempus bibendum.</p>
+                    </div>
+                </div>
+                <div>
+                    <p>1 day ago</p>
+                    <div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum et ligula vulputate vehicula. Aenean pharetra arcu bibendum tempus bibendum.</p>
+                    </div>
+                </div>
+                <div>
+                    <p>1 day ago</p>
+                    <div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum et ligula vulputate vehicula. Aenean pharetra arcu bibendum tempus bibendum.</p>
+                    </div>
+                </div>
+            </div>
+            <!-- <hr> -->
+            <div id="bottom-dashboard">
+                <div id='send'>
+                    <textarea placeholder="Your message"></textarea>
+                </div>
+                <div>
+                    <input type="file">
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
